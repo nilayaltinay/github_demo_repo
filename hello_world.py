2ca1a45dcd7bb770980d7b69f0a6a7420a1fbe51
nilayaltinay <94836516+nilayaltinay@users.noreply.github.com>
2022-07-10 15:23:24 +1000

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,13 @@
 print ('hello World!')
+
+while True:
+    try:
+        age = int(input("Enter your age: "))
+    except ValueError:
+        print("This is not a valid age.")
+        continue
+    else:
+        break
+
+
+print(f"You are {age} years old")
